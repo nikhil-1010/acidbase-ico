07ec8b1acfecfed67e4c4ff8d1e9bc1ab68d60d3
nikyogi-333 <nikyogi333@gmail.com>
2023-06-27 23:01:53 +0530

    set migration

---
+++ database/migrations/2023_06_27_171935_create_transaction_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transaction', function (Blueprint $table) {
+            $table->id();
+            $table->string('trx_id');
+            $table->string('investor_address');
+            $table->double('paid_amount',20,8);
+            $table->double('token_amount',50,8);
+            $table->integer('sale_type');
+            $table->dateTime('created_at')->useCurrent();
+            $table->dateTime('updated_at')->useCurrent()->useCurrentOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transaction');
+    }
+};
